5272c35859a264103a22e5fe4c3f932abaf88a85
anshusharma26 <42262910+anshusharma26@users.noreply.github.com>
2019-05-01 23:55:36 +0530

    Add files via upload

---
+++ pyramid.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+int i,j,n,num=1,k;
+system("clear");
+printf("enter the number of lines:");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+ for(j=n-1;j>=i;j--)
+{
+printf("  ");
+}
+ for(k=1;k<=i;k++)
+ {
+  printf("%d",num);
+ printf("  ");
+  num++; 
+}
+printf(" \n ");
+}
+}
+
